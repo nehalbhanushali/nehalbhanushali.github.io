a4f755c1ce639f16f5aea0e1f0fbe98d51cb60a7
Intern <intern@Interns-MacBook-Pro.local>
2016-07-27 16:02:04 -0500

    TheCookBot added with react-facebook-login

---
+++ TheCookBot/src/Merge.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import FacebookLogin from 'react-facebook-login';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import { Grid, Row, Col, Clearfix } from 'react-bootstrap';
+
+class Page extends React.Component {
+
+constructor(props){
+        super(props);
+        this.state = {
+         
+         userData : []
+
+        }
+      //  this.handlePost = this.handlePost.bind(this);
+
+    }
+
+
+responseFacebook(response){
+
+   console.log(JSON.stringify(response) + " "+response.status);
+  if (response.name.length >0) {
+    // Logged into your app and Facebook.
+  document.getElementById('status').innerHTML =
+    'Thanks for logging in, ' + response.name + '!';
+
+    this.setState({userData: response});
+    //this.props.userLoginInfo(response);
+  } else if (response.status === 'not_authorized') {
+    // The person is logged into Facebook, but not your app.
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into this app.';
+  } else {
+    // The person is not logged into Facebook, so we're not sure if
+    // they are logged into this app or not.
+    document.getElementById('status').innerHTML = 'Please log ' +
+    'into Facebook.';
+  }
+
+}
+
+ 
+render(){
+
+  var appContent =  <FacebookLogin
+    appId="1229974300360018"
+    autoLoad={true}
+    callback={this.responseFacebook} />;
+
+return (<Col xs={12} sm ={10} smOffset = {1} md={8} mdOffset={2} lg = {6} lgOffset={3}>
+  
+<Card>
+
+ <CardHeader
+      title="TheCookBot"
+      subtitle="Recipe by ingredients and more.."
+      avatar="http://lorempixel.com/100/100/food/"
+    />
+ <CardActions>
+ 
+{appContent}
+  
+   </CardActions> 
+    <CardText>
+    <div id="status"></div>
+    </CardText>
+}
+</Card>
+
+</Col>);
+}
+
+}
+
+
+export default Page;
---
+++ TheCookBot/src/index.js
@@ -0,0 +1,71 @@
+import { AppContainer } from 'react-hot-loader';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {red300,red200,red100,pink300,pink400,pink500,indigo500, indigo700, grey900, grey800, grey700, grey100, indigo300, grey300, grey400, grey500,pinkA200,white, darkBlack, fullBlack, cyan500, green700} from 'material-ui/styles/colors';
+import {fade} from 'material-ui/utils/colorManipulator';
+
+import Page from './Merge.js';
+
+const muiTheme = getMuiTheme({
+  spacing: {
+    iconSize: 24,
+    desktopGutter: 24,
+    desktopGutterMore: 32,
+    desktopGutterLess: 16,
+    desktopGutterMini: 8,
+    desktopKeylineIncrement: 64,
+    desktopDropDownMenuItemHeight: 32,
+    desktopDropDownMenuFontSize: 15,
+    desktopDrawerMenuItemHeight: 48,
+    desktopSubheaderHeight: 48,
+    desktopToolbarHeight: 56,
+  },
+  fontFamily: 'Roboto, sans-serif',
+  palette: {
+    primary1Color: grey700,
+    primary2Color: grey800,
+    primary3Color: grey900,
+    accent1Color: pink300,
+    accent2Color: pink400,
+    accent3Color: pink500,
+    textColor: darkBlack,
+    alternateTextColor: white,
+    canvasColor: white,
+    borderColor: grey300,
+    disabledColor: fade(darkBlack, 0.3),
+    pickerHeaderColor: cyan500,
+    clockCircleColor: fade(darkBlack, 0.07),
+    shadowColor: fullBlack,
+  },
+});
+const rootElement = document.getElementById('root');
+ 
+ 
+ReactDOM.render(
+  
+    <MuiThemeProvider muiTheme={muiTheme}>
+    <AppContainer>
+   <Page />
+   </AppContainer>
+  </MuiThemeProvider>,
+  rootElement
+);
+
+if (module.hot) {
+  module.hot.accept('./Merge', () => {
+    const NextApp = require('./Merge').default;
+
+
+    ReactDOM.render(
+      <MuiThemeProvider muiTheme={muiTheme}>
+      <AppContainer>
+         <NextApp />
+      </AppContainer>
+      </MuiThemeProvider>,
+      rootElement
+    );
+  });
+}
+
